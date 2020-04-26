2f02e2829ba44a5030d6894aab1617211dbeb038
Igo Pereira Barros <igorestacioceut@gmail.com>
2020-04-26 11:33:07 -0300

    removendo credenciais pessoais

--- streamlit-docker/src/settings.py
+++ streamlit-docker/src/settings.py
@@ -1,13 +1,13 @@
 
 
 CONFIG_MONGODB = {
-	'MONGO_URI': 'mongodb+srv://admin-mongodb:54321@clusterprojectmongodb-sztyp.mongodb.net/test?retryWrites=true&w=majority',
-	'MONGO_DB': 'codenation_tutorial',
-	'MONGO_COLLECTIONS': 'house_price'
+	'MONGO_URI': '',
+	'MONGO_DB': '',
+	'MONGO_COLLECTIONS': ''
 }
 
 CONFIG_GMAIL = {
-	'SENDER_MAIL': 'suportestreamlit@gmail.com',
-	'PASSWORD': 'streamlitds2020',
-	'RECEIVER_MAIL': 'suportestreamlit@gmail.com'
+	'SENDER_MAIL': '',
+	'PASSWORD': '',
+	'RECEIVER_MAIL': ''
 }
